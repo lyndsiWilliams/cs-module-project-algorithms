d92d192450bc7e277a9d45a73fa55bde5c1ca360
lyndsiWilliams <kcatgirl@gmail.com>
2020-07-22 17:45:53 -0500

    eating_cookies (small) successfully implemented - MVP achieved!

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,23 @@ Returns: an integer
 def eating_cookies(n):
     # Your code here
 
-    pass
+    # Check that input is negative
+    if n < 0:
+        # Can't eat negative cookies!
+        return 0
+    # Check if input is 0
+    if n == 0:
+        # Only 1 way to eat 0 cookies!
+        return 1
+    # Recursively run through and add the return for each scenario
+    # (Eating 1, 2, or 3 cookies)
+    else:
+        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+
+        
+
+
+        
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
