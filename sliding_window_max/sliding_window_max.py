07a969eacabb1e4048f148dbe67431129e8a2061
lyndsiWilliams <kcatgirl@gmail.com>
2020-07-22 16:36:05 -0500

    sliding_window_max successfully implemented

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -5,7 +5,24 @@ Returns: a List of integers
 def sliding_window_max(nums, k):
     # Your code here
 
-    pass
+    # Check if numbers array exists
+    if nums:
+        # Set an empty list for the max numbers to live
+        max_numbers = []
+
+        # Loop through the list of numbers
+        for i in range(len(nums)):
+            # If k is <= the length of the numbers list
+            if k <= len(nums):
+                # Set the window to start at the current index and
+                # end 'k' elements over
+                window = nums[i:k]
+                # Move the window up one position
+                k += 1
+                # Append the max number to the max_numbers array
+                max_numbers.append(max(window))
+
+    return max_numbers
 
 
 if __name__ == '__main__':
